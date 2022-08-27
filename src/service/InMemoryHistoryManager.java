b51fc43fc8c91722f908500e897560d7476cabfd
Saeldrit <pavlovskiy.al93@gmail.com>
2022-08-27 09:56:34 +0300

    fix: adjust the method of adding tasks

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -23,34 +23,20 @@ public class InMemoryHistoryManager implements HistoryManager {
         this.limitSize = limitSize;
     }
 
-    /**
-     *
-     * Сначала делаю удаление, чтобы старый просмотр удалился, а новый добавился на свое место.
-     * Иначе выйдет так, что при вводе 1 - 2 - 3 - 1 - 3 история будет хранить = 1 - 2 - 3,
-     * когда правильный результат 2 - 1 - 3. Я изначально хотел LinkedHashSet использовать,
-     * но сначала не додумался об простом удалении и после добавлении, чтобы выстроить правильный порядок
-     * добавления, поэтому решил сам написать смесь стека и множества. Херня вышла, а может не совсем,
-     * но в итоге переделал вот таким решением.
-     */
     @Override
     public void add(Task task) {
-        tasks.remove(task);
+        if (tasks.size() < limitSize) {
+            tasks.remove(task);
+        } else {
+            if (!tasks.remove(task)) {
+                tasks.remove(tasks.iterator().next());
+            }
+        }
         tasks.add(task);
     }
 
     @Override
     public List<Task> getHistory() {
-        if (tasks.size() < limitSize && tasks.size() > 0) {
-            return new ArrayList<>(tasks);
-        } else if (tasks.size() > limitSize) {
-            return getTasksByLimitSize();
-        }
-        return new ArrayList<>();
-    }
-
-    private List<Task> getTasksByLimitSize() {
-        int startIndex = tasks.size() - limitSize;
-
-        return new ArrayList<>(tasks).subList(startIndex, tasks.size());
+        return new ArrayList<>(tasks);
     }
 }
