503f2eb8cdde944bee5fc0ddbfe48e3c20a0e233
Saeldrit <pavlovskiy.al93@gmail.com>
2022-10-17 19:10:08 +0300

    feat: add Lombok and new Test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,4 @@
-import model.Epic;
-import model.Subtask;
-import model.Task;
-import model.status.Status;
-import service.FileBackedTasksManager;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
-
 public class Main {
     public static void main(String[] args) {
-        FileBackedTasksManager manager = new FileBackedTasksManager("src/main/resources/tasks.csv");
-        Task task1 = Task.builder().id(1)
-                .title("Task1")
-                .description("description")
-                .status(Status.NEW)
-                .duration(15L)
-                .startTime(LocalDateTime.now().plusMinutes(20))
-                .build();
-
-        Set<Task> prioritizedTasks = new TreeSet<>(Comparator
-                .comparing(Task::getStartTime,
-                        Comparator.nullsLast(
-                                Comparator.naturalOrder()))
-                .thenComparing(Task::getId));
-
-        prioritizedTasks.add(task1);
-        task1.setStatus(Status.DONE);
-        task1.setStartTime(LocalDateTime.now());
-        prioritizedTasks.add(task1);
-        prioritizedTasks.forEach(System.out::println);
     }
 }
