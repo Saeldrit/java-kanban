fe07febf4169909ad8f038bc1b525dbe165443d2
Saeldrit <pavlovskiy.al93@gmail.com>
2022-11-21 12:51:53 +0300

    style: main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 import factory.Managers;
 import http.HttpTaskServer;
 
+/**
+ * Привет! Работа сделана не до конца, просто хочу знать, на правильном ли я пути.
+ * На данный момент запушил только HttpTaskServer с endpoint.
+ */
+
 public class Main {
     public static void main(String[] args) {
         HttpTaskServer httpTaskServer = new HttpTaskServer(Managers.getFileBacked("src/main/resources/tasks.csv"));
