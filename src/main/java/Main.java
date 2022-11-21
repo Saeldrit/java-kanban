7e99f1a627af06701c41f4d42a6dc6f185062633
Saeldrit <pavlovskiy.al93@gmail.com>
2022-11-21 12:49:05 +0300

    style: main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,9 @@
 import factory.Managers;
 import http.HttpTaskServer;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         HttpTaskServer httpTaskServer = new HttpTaskServer(Managers.getFileBacked("src/main/resources/tasks.csv"));
         httpTaskServer.startServer();
-
-
     }
 }
